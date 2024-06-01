43d8c98787262d22464213e78df02186c2c1cbd3
Carl-ZLJ <zljmms32@gmail.com>
2024-06-01 14:31:44 +0800

    fix

--- .config/waybar/test-vpn.sh
+++ .config/waybar/test-vpn.sh
@@ -2,6 +2,7 @@
 
 address="https://www.google.com"
 proxy="https:http://127.0.0.1:7890"
+# as specific as possible
 response=`xh get -h --timeout 5 --proxy ${proxy} ${address} 2>&1`
 
 if echo "$response" | grep "error" 1>/dev/null 2>/dev/null; then
