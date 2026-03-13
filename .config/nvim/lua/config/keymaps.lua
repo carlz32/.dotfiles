ae3848a3f591ad216045b9e276d837950ce1babf
carlz32 <zljmms32@gmail.com>
2026-03-13 18:48:42 +0800

    remove nvim submodule

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,25 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.g.mapleader = " "
+
+-- Normal
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit Insert Mode" })
+vim.keymap.set("n", "<Esc>", vim.cmd.nohl, { desc = "No Highlight" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit Terminal Mode" })
+
+-- Smart copy and paste
+vim.keymap.set({ "n", "v" }, "x", "\"_x", { desc = "Cut without Yank" })
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d", { desc = "Delete without Yank" })
+vim.keymap.set({ "n", "v" }, "<leader>p", "\"_dP", { desc = "Paste without Yank" })
+
+vim.keymap.set("n", "J", "mzJ`z", { desc = "Smart Join" })
+
+-- Move Lines
+vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
